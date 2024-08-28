b6d938d83e02e196a8930d2e6527f6888f5b9da1
Redox <140275343+oxo4real@users.noreply.github.com>
2024-08-28 17:57:51 +0100

    added the UI for the game

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,59 +1,74 @@
-const choices = ["rock", "paper", "scissors"];
+document.addEventListener("DOMContentLoaded", function() {
+	const choices = ["rock", "paper", "scissors"];
 
-let humanScore = 0;
-let computerScore = 0;
+	let humanScore = 0;
+	let computerScore = 0;
 
-function getComputerChoice() {
-	let choice_index = Math.floor(Math.random() * 3);
-	return (choices[choice_index]);
-}
+	function getComputerChoice() {
+		let choice_index = Math.floor(Math.random() * 3);
+		return (choices[choice_index]);
+	}
 
-function getHumanChoice() {
-	let choice = String(prompt("What will you play?").toLowerCase());
-	if (!choices.includes(choice)) {
-		do {
-			choice = String(prompt("Please enter a valid move (either Rock or Paper or Scissors)").toLowerCase());
+	function getHumanChoice() {
+		let choice = String(prompt("What will you play?").toLowerCase());
+		if (!choices.includes(choice)) {
+			do {
+				choice = String(prompt("Please enter a valid move (either Rock or Paper or Scissors)").toLowerCase());
+			}
+			while (!choices.includes(choice));
 		}
-		while (!choices.includes(choice));
+		return (choice);
 	}
-	return (choice);
-}
 
-function playRound(humanChoice, computerChoice) {
-	const hc_index = choices.indexOf(humanChoice);
-	const cc_index = choices.indexOf(computerChoice);
-	if (hc_index === 0 && cc_index === 2) {
-		humanScore += 1;
-		console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-	} else if (cc_index === 0 && hc_index === 2) {
-		computerScore += 1;
-		console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-	} else if (hc_index > cc_index) {
-		humanScore += 1;
-		console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-	} else if (hc_index < cc_index) {
-		computerScore += 1;
-		console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-	} else {
-		console.log("Tie!");
-	}
-}
+	let header = document.querySelector("#header");
+	let robotchoice = document.querySelector("#robotchoice");
+	let humanchoice = document.querySelector("#humanchoice");
 
-function playgame() {
-	humanScore = 0;
-	computerScore = 0;
-	for (let i = 0; i < 5; i++) {
-		const humanSelection = getHumanChoice();
-		const computerSelection = getComputerChoice();
-		playRound(humanSelection, computerSelection);
-	}
-	if (humanScore < computerScore) {
-		console.log("The Computer wins this game!");
-	} else if (humanScore > computerScore) {
-		console.log("The Human wins this game!");
-	} else {
-		console.log("This game has no winner. It's a Tie!");
-	}
-}
+	function playRound(hchoice, rchoice) {
+		const hc_index = choices.indexOf(hchoice);
+		const cc_index = choices.indexOf(rchoice);
 
-playgame()
+		if (hc_index === 0 && cc_index === 2) {
+			humanScore += 1;
+			header.innerHTML = "You Win!";
+		} else if (cc_index === 0 && hc_index === 2) {
+			computerScore += 1;
+			header.innerHTML = "You Lose";
+		} else if (hc_index > cc_index) {
+			humanScore += 1;
+			header.innerHTML = "You Win!";
+		} else if (hc_index < cc_index) {
+			computerScore += 1;
+			header.innerHTML = "You Lose";
+		} else {
+			header.innerHTML = "Tie!";
+		}
+
+		if (rchoice == "rock") {
+			robotchoice.innerHTML ='<i class="fa-regular fa-hand-back-fist"></i>';
+		} else if (rchoice == "paper") {
+			robotchoice.innerHTML = '<i class="fa-regular fa-hand"></i>';
+		} else {
+			robotchoice.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+		}
+
+		if (hchoice == "rock") {
+			humanchoice.innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+		} else if (hchoice == "paper") {
+			humanchoice.innerHTML = '<i class="fa-regular fa-hand"></i>';
+		} else {
+			humanchoice.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+		}
+
+		let choicess = document.querySelector("#choices");
+		choicess.innerHTML = '<button id="replay" onclick="location.reload()">Replay</button>';
+	}
+
+	let rock = document.querySelector("#rock");
+	let paper = document.querySelector("#paper");
+	let scissors = document.querySelector("#scissors");
+
+	rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+	paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+	scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+})
